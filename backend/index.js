62d0103af44218d05864cde384723591b6f709c2
Stanley Chibuike <chibuike.stanley50@gmail.com>
2021-10-09 21:39:16 +0100

    App ready

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const indexRoutes = require('./routes/index')
 const {localPort} = require('./bin/config');
 const dbConfiguration = require('./bin/db');
